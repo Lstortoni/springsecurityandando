42a39a454bb61036b304408ccea549a043818deb
Lstortoni <89618746+Lstortoni@users.noreply.github.com>
2024-05-29 21:52:52 -0300

    commit primero

---
+++ src/main/java/com/citycode/spring_securityfinalterminado/SpringSecurityfinalterminadoApplication.java
@@ -0,0 +1,13 @@
+package com.citycode.spring_securityfinalterminado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityfinalterminadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityfinalterminadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citycode/spring_securityfinalterminado/SpringSecurityfinalterminadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citycode.spring_securityfinalterminado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityfinalterminadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
